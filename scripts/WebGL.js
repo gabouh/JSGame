04f94dd7b801628c3f14d65eda384a93da9f6dfd
Jan Polak <janpolak.lysa@gmail.com>
2017-01-19 15:28:52 +0100

    More WebGL work, new editor design prep

--- scripts/WebGL.js
+++ scripts/WebGL.js
@@ -2,29 +2,28 @@
  * Created by polakja15 on 12.01.2017.
  */
 var fragShader = "precision mediump float;\n" +
-    "uniform vec2 resolution;\n" +
+    "uniform sampler2D texture;\n" +
+    "varying highp vec2 vUV;\n" +
     "void main() {\n" +
-    "   vec2 uv = (resolution.xy - gl_FragCoord.xy) / max(resolution.x, resolution.y);" +
-    "   gl_FragColor = vec4(0.0, uv, 1.0);\n" +
+    "   gl_FragColor = texture2D(texture, vUV);\n" +
     "}";
-var fragShader2 = "precision mediump float;\n" +
-    "uniform vec2 resolution;\n" +
-    "void main() {\n" +
-    "   vec2 uv = (resolution.xy - gl_FragCoord.xy) / max(resolution.x, resolution.y);" +
-    "   gl_FragColor = vec4(uv, 0.0, 1.0);\n" +
-    "}";
-var vertShader = "attribute vec3 vPos;\n" +
+var vertShader = "attribute vec3 aVertexPos;\n" +
+    "attribute vec2 aUVPos;\n" +
+    "varying highp vec2 vUV;\n" +
     "void main(void) {\n" +
-    "   gl_Position = vec4(vPos, 1.0);\n" +
+    "   gl_Position = vec4(aVertexPos, 1.0);\n" +
+    "   //vUV = aUVPos;\n" +
     "}";
+
 function WebGL(canvas) {
     this.canvas = canvas;
     this.buffers = {};
     this.gl = canvas.getContext("webgl");
     this.shaders = {};
-    this.textureAtlas = null;
+    this.textures = {};
     this.ctrB = 0;
     this.ctrS = 0;
+    this.ctrT = 0;
 
     this.init = function () {
         this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -33,11 +32,14 @@ function WebGL(canvas) {
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
         var shader = this.loadShader(fragShader, vertShader);
-        this.loadBuffer([1.0,1.0,0.0,-1.0,1.0,0.0,1.0,-1.0,0.0,-1.0,-1.0,0.0], shader);
-        this.loadBuffer([0.2,0.2,0.0,-0.2,0.2,0.0,0.2,-0.2,0.0,-0.2,-0.2,0.0], this.loadShader(fragShader2, vertShader));
+        this.loadBuffer([1,1,0, -1,1,0, 1,-1,0, -1,-1,0], [1,1, -1,1, 1,-1, -1,-1], this.loadTexture("assets/back.png"), shader);
     };
 
     this.draw = function () {
+        if (this.texturesToLoad > 0) {
+            console.log("Textures not loaded yet!");
+            return;
+        }
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
         for (var bufferID in this.buffers) {
@@ -48,29 +50,29 @@ function WebGL(canvas) {
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer.vertexBuffer);
             this.gl.vertexAttribPointer(shader.vertexPos, 3, this.gl.FLOAT, false, 0, 0);
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer.uvBuffer);
-            this.gl.vertexAttribPointer(shader.uvCoord, 2, gl.FLOAT, false, 0, 0);
+            this.gl.vertexAttribPointer(shader.uvCoord, 2, this.gl.FLOAT, false, 0, 0);
 
             this.gl.activeTexture(this.gl.TEXTURE0);
-            this.gl.bindTexture(this.gl.TEXTURE_2D, this.atlasTexture);
-            this.gl.uniform1i(this.gl.getUniformLocation(shader.shader, "texture"), 0);
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[buffer.textureID]);
+            this.gl.uniform1i(this.gl.getUniformLocation(shader, "texture"), 0);
 
             this.gl.uniform2fv(this.gl.getUniformLocation(shader, "resolution"), Float32Array.from([this.canvas.height, this.canvas.width]));
 
             this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
         }
     };
-    
+
     this.loadShader = function (fragShader, vertShader) {
         var frag = this.compileShader(fragShader, this.gl.FRAGMENT_SHADER);
         var vert = this.compileShader(vertShader, this.gl.VERTEX_SHADER);
         var program = this.createShaderProgram(frag, vert);
         this.gl.useProgram(program);
-        var vPos = this.gl.getAttribLocation(program, "vPos");
-        this.gl.enableVertexAttribArray(vPos);
-        var uvCoord = gl.getAttribLocation(shaderProgram, "uvCoord");
-        this.gl.enableVertexAttribArray(textureCoordAttribute);
+        var aVertexPos = this.gl.getAttribLocation(program, "aVertexPos");
+        this.gl.enableVertexAttribArray(aVertexPos);
+        var uvCoord = this.gl.getAttribLocation(program, "aUVPos");
+        this.gl.enableVertexAttribArray(uvCoord);
         var id = this.ctrS++;
-        this.shaders[id] = new Shader(vPos, uvCoord, program);
+        this.shaders[id] = new Shader(aVertexPos, uvCoord, program);
         return id;
     };
     
@@ -99,15 +101,38 @@ function WebGL(canvas) {
 
     this.loadBuffer = function (vertices, uv, textureID, shader) {
         var vertexBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
         var uvBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, uvBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(uv), this.gl.STATIC_DRAW);
         var ret = this.ctrB++;
         this.buffers[ret] = new Buffer(vertexBuffer, uvBuffer, shader, textureID);
         return ret;
     };
+
+    this.texturesToLoad = 0;
+
+    this.loadTexture = function (path) {
+        this.texturesToLoad++;
+        var image = new Image();
+        var id = this.ctrT++;
+        image.onload = function () {
+            var texture = this.gl.createTexture();
+            this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+            this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
+            this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+            this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR_MIPMAP_NEAREST);
+            this.gl.generateMipmap(this.gl.TEXTURE_2D);
+            this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+            this.textures[id] = texture;
+            this.texturesToLoad--;
+        }.bind(this);
+        image.src = path;
+        console.log(this.ctrT);
+        return id;
+    };
+
     this.init();
 }
 
